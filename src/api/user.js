82d51f82931f2b82a042e041664ac58c018d0450
“zaimakankanb” <31170583+XIGUAXIE@users.noreply.github.com>
2024-08-07 23:11:53 +0800

    Update user.js

--- src/api/user.js
+++ src/api/user.js
@@ -2,24 +2,29 @@
 
 import axios from 'axios';
 
-const API_URL = 'https://whoscat.robolink.ai/users'; // 确保与后端的URL一致
+const API_URL = 'https://whoscat.robolink.ai/users';
+const API_KEY = 'Wat1rm11on.whoscat';
 
 export const getUserById = async (id) => {
   try {
-    const response = await axios.get(`${API_URL}/${id}`);
+    const response = await axios.get(`${API_URL}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`
+      }
+    });
     return response.data;
   } catch (error) {
-    if (error.response && error.response.status === 404) {
-      // 用户不存在时返回 null
-      return null;
-    }
     throw error;
   }
 };
 
 export const createUser = async (id) => {
   try {
-    const response = await axios.post(API_URL, { id });
+    const response = await axios.post(API_URL, { id }, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`
+      }
+    });
     return response.data;
   } catch (error) {
     throw error;
